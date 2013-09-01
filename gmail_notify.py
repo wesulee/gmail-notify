2b6d6a7d6580b3b2a8c46b86948ac8a31968fd5e
Wesley Millar <w3sleym@gmail.com>
2013-09-01 16:02:57 -0700

    fixed error parsing address, rewrote re-encoder, minor improvements

--- gmail_notify.py
+++ gmail_notify.py
@@ -11,13 +11,39 @@ from email import message_from_string
 from email.mime.text import MIMEText
 
 
+def truncate(string, length, omission):
+    '''shorten string to length, appending omission to end if shortened'''
+    if len(string) <= length:
+        return string
+    else:
+        return string[:length - len(omission)] + omission
+
+
+def re_encode(string, encoding='ascii'):
+    '''return string encoded as encoding
+    string may have multiple encodings
+    (does not guarantee return preserves whitespace)
+    '''
+    def encode(string, encoding, re_encode):
+        if encoding is None:
+            return string
+        else:
+            return string.decode(encoding).encode(re_encode, 'ignore')
+
+    re_encoded = []
+    for string, str_encoding in decode_header(string):
+        re_encoded.append(encode(string, str_encoding, encoding))
+    return ' '.join(re_encoded)
+
 
 class Notifier(object):
     def __init__(self, email, password, notifyEmail, settingsDict):
         self.notifyEmail = notifyEmail
         self.settings = settingsDict
         self.login(email, password)
-        self.setUIDs()
+        self.UIDs = self.getUIDs()
+        # subject of notification email
+        self.notifySubject = 'Generated Notification'
 
     def login(self, email, password):
         M = imaplib.IMAP4_SSL('imap.gmail.com', '993')
@@ -27,7 +53,8 @@ class Notifier(object):
             self.loginCred = (email, password)
             self.M = M
         except Exception as e:
-            print "Unable to login:", e
+            print "Unable to login!"
+            raise e
 
     def getUIDs(self):
         result, data = self.M.uid('search', None, 'ALL')
@@ -36,53 +63,52 @@ class Notifier(object):
         else:
             print "Bad response getting UIDs"
 
-    def setUIDs(self):
-        '''gets UIDs for currently selected folder and saves them'''
-        self.UIDs = self.getUIDs()
+    def getEmail(self, uid):
+        '''get a raw email using the uid'''
+        result, data = self.M.uid('fetch', uid, '(RFC822)')
+        if result == 'OK' and data[0] is not None:
+            return data[0][1]
+        else:
+            print 'Error retrieving email uid: %s' % uid
 
-    def genEmails(self):
-        '''raw email generator for inbox'''
+    def genAllEmails(self):
+        '''raw email generator for all retrievable emails in folder'''
         for uid in self.UIDs:
-            result, data = self.M.uid('fetch', uid, '(RFC822)')
-            yield data[0][1]
+            rawEmail = self.getEmail(uid)
+            if rawEmail is not None:
+                yield rawEmail
 
     def formatDate(self, string):
         '''returns the date/time email received into current time
-        returns (date, time), date=m/dd, time=hh:mm AM/PM'''
-        t = time.mktime(parsedate(string.split('\n')[1].strip()))
+        returns (date, time), date=mm/dd, time=hh:mm AM/PM'''
+        t = time.mktime(parsedate(string.split(';')[1].strip()))
         tStr = time.strftime("%m/%d %I:%M %p", time.localtime(t))
-        if int(tStr[:2]) < 10:
-            tStr = tStr[1:]     # remove the 0 from month
         tSplit = tStr.split()
         return tSplit[0], ' '.join(tSplit[1:])
 
-    def decodeSubject(self, string):
-        '''decodes subject to ascii, removing non-ascii characters'''
-        subject, encoding = decode_header(string)[0]
-        if encoding == None:
-            return subject
-        return subject.decode(encoding).encode('ascii', 'ignore')
+    def getEmailInfo(self, rawEmail):
+        '''given a raw email, returns (from, date, time, subject)'''
+        email = message_from_string(rawEmail)
+        date, time = self.formatDate(email['Received'])
+        subject = re_encode(email['Subject'])
+        return (re_encode(email['From']), date, time, subject)
 
-    def getEmailInfo(self):
-        emailData = []          # (from, date, time, subject)
-        for emRaw in self.genEmails():
-            em = message_from_string(emRaw)
-            date, time = self.formatDate(em['Received'])
-            subject = self.decodeSubject(em['Subject'])
-            emailData.append((em['From'], date, time, subject))
+    def getAllEmailInfo(self):
+        '''get the info of every email in folder'''
+        emailData = []
+        for rawEmail in self.genAllEmails():
+            emailData.append(self.getEmailInfo(rawEmail))
         return emailData
 
-    def createNotification(self, incName=False, incDate=False, subjectLen=35):
+    def createNotification(self, incName=False, incDate=False):
         text = ''
-        for address, date, time, subject in self.getEmailInfo():
+        for address, date, time, subject in self.getAllEmailInfo():
             tmpText = []
             if not incName:                 # remove name from address
                 if address.find('<') != -1:
                     address = address[address.index('<') + 1:][:-1]
             tmpText.append(address)
-            if len(subject) > subjectLen:   # shorten subject if too long
-                subject = subject[:subjectLen - 2] + '...'
-            tmpText.append(subject)
+            tmpText.append(truncate(subject, 35, '...'))
             if incDate:             # include date of email in notification
                 tmpText.append(date)
             text += ' '.join(tmpText + [time]) + '\n'
@@ -104,7 +130,7 @@ class Notifier(object):
         msg = MIMEText(content)
         msg['From'] = self.loginCred[0]
         msg['To'] = self.notifyEmail
-        msg['Subject'] = 'Generated Notification'
+        msg['Subject'] = self.notifySubject
         server.sendmail(self.loginCred[0], self.notifyEmail, msg.as_string())
         server.quit()
 
@@ -115,7 +141,7 @@ class Notifier(object):
 
     def deleteNotification(self):
         self.M.select('[Gmail]/Sent Mail')
-        search = '(SUBJECT "Generated Notification"'
+        search = '(SUBJECT "%s"' % self.notifySubject
         search += ' TO "%s")' % self.notifyEmail
         result, data = self.M.uid('search', None, search)
         self.M.uid('store', data[0].split()[-1], '+FLAGS', '\\Deleted')
